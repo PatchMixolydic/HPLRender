c15c40228576ee28f9c9467e3ccbf578b5a33508
Ruby Lord <nipkraps@gmail.com>
2019-05-01 17:56:02 -0500

    analyze: We can now get min, max, average, and best bin on average
    This allows for analysis of best-performing values allowing for rapid
    optimization of HPL (as fast as HPL will finish, at least). For instance,
    binning the The Grand Cluster NB test results by NB and using getBestBin
    to get the fastest time on average lets us see that the optimal NB for
    a single core run on The Grand Cluster is 216, with our assumption, 224,
    coming close to the time achieved by 216.

--- analyze.py
+++ analyze.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
+import statistics
 from collections import defaultdict
-from HPLResult import HPLResult
 import rend
+from HPLResult import HPLResult
+
+# For minMaxAvgPerBin
+MMAMin = 0
+MMAMax = 1
+MMAAvg = 2
 
 def binResultsBy(results, binningFunction):
     """
@@ -15,3 +21,32 @@ def binResultsBy(results, binningFunction):
     for result in results:
         binnedResults[binningFunction(result)].append(result) # using dict.get() here breaks the defaultdict behaviour
     return dict(binnedResults) # cast to dict - we don't need the default entry functionality anymore
+
+def minMaxAvgPerBin(binnedResults, statFunction):
+    """
+    For each bin in the binned results, find the minimum, maximum, and average of the value of the results determined
+    by statFunction. The constants MMAMin, MMAMax, and MMAAvg are meant for accessing the results tuple data.
+    :param binnedResults: The binned HPLResults.
+    :param statFunction: A function that will return a value given an HPLResult. This will most likely be a getter from
+    HPLResult.
+    :return: A dictionary containing a tuple with the min, max, and average value of the bin as given by statFunction.
+    """
+    results = {}
+    for key, contents in binnedResults.items(): # Loop over all of the bins.
+        contents = list(map(statFunction, contents)) # Process their contents with statFunction.
+        results[key] = (min(contents), max(contents), statistics.mean(contents)) # Minimum, maximum, average
+    return results
+
+def getBestBin(binnedResults, statFunction, lowerIsBetter):
+    """
+    Gets the best bin based on the average return value of statFunction for the items in that bin.
+    :param binnedResults: The binned HPLResults.
+    :param statFunction: A function that will return a value based on an HPLResult. See also minMaxAvgPerBin.
+    :param lowerIsBetter: Is a lower score preferable?
+    :return: The key value for the best bin on average for the given statFunction.
+    """
+    minMaxAvg = minMaxAvgPerBin(binnedResults, statFunction)
+    bins = list(minMaxAvg.keys())
+    # Sort the bins based on their average values, where the 0th element is the best.
+    bins.sort(key = lambda x: minMaxAvg.get(x)[MMAAvg], reverse = not lowerIsBetter)
+    return bins[0] # return the 0th element, which is the best
