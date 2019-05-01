5021afe73b9fc7e41b0e60c5e3f65d2607aab718
Ruby Lord <nipkraps@gmail.com>
2019-05-01 17:21:47 -0500

    analyze: Add results binning functionality
    It is now possible to easily analyze HPL results. I will be writing more
    thorough analysis tools, of course, but this is a good start.

--- HPLResult.py
+++ HPLResult.py
@@ -10,8 +10,8 @@ class HPLResult:
         self.q = q
         self.time = time
         self.gflops = gflops
-        self.start = start
-        self.end = end
+        self.startTime = start
+        self.endTime = end
 
     def __str__(self):
         output = (
@@ -22,3 +22,33 @@ class HPLResult:
             "end time {}\n"
         ).format(self.encodedTime, self.n, self.nb, self.p, self.q, self.time, self.gflops, self.start, self.end)
         return output
+
+    """
+    The following getters are meant to be used for organizing HPLResults into bins.
+    This is meant to provide a cleaner way to get attributes dynamically versus getattr(obj, "attribute")
+    There are no corresponding setters - set the values directly instead if you must.
+    """
+
+    def getEncodedTime(self):
+        return self.encodedTime
+
+    def getN(self):
+        return self.n
+
+    def getNB(self): # rights
+        return self.nb
+
+    def getP(self):
+        return self.p
+
+    def getTime(self):
+        return self.time
+
+    def getGflops(self):
+        return self.gflops
+
+    def getStartTime(self):
+        return self.startTime
+
+    def getEndTime(self):
+        return self.endTime
--- analyze.py
+++ analyze.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python3
+from collections import defaultdict
+from HPLResult import HPLResult
 import rend
 
-
+def binResultsBy(results, binningFunction):
+    """
+    Place results into bins depending on a binning function.
+    :param results: A list of results to bin.
+    :param binningFunction: The function to use to sort results into bins. You can use the getters in HPLResult for this.
+    For example, to bin functions by NB, one should use HPLResult.getNB for the binningFunction.
+    :return: A dictionary containing a list of results mapped to key values by the result of the binning function.
+    """
+    binnedResults = defaultdict(list) # if an invalid key is accessed, it defaults to being an empty list
+    for result in results:
+        binnedResults[binningFunction(result)].append(result) # using dict.get() here breaks the defaultdict behaviour
+    return dict(binnedResults) # cast to dict - we don't need the default entry functionality anymore
